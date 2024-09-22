b0cf6f5770937e3fe010240b37c010fbcb7133a5
Tilwa <shahrukhaltaf123@gmail.com>
2024-09-22 23:31:35 +0500

    db connected

--- backend/app.js
+++ backend/app.js
@@ -1,22 +1,8 @@
-// app.js
 const express = require("express");
-const dotenv = require("dotenv");
-
-// Load environment variables from .env file
-dotenv.config();
-
 const app = express();
-const port = process.env.PORT || 5000;
 
-// Middleware to parse JSON data
-app.use(express.json());
-
-// Sample route
-app.get("/", (req, res) => {
-  res.send("Welcome to Vocabulary Builder API!");
+app.get("/test", (req, res, next) => {
+  res.json("this test route is working");
 });
 
-// Start the server
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});
+module.exports = app;
